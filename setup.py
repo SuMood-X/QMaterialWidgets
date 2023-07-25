4a7b4faa4d6c4d8d7338d18400ffed837d37620d
zhiyiYo <1319158137@qq.com>
2023-07-26 07:59:47 +0800

    修复打包没有 material_color 模块的问题

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setuptools.setup(
     name="PySide6-Material-Widgets",
-    version="0.0.1",
+    version="0.0.2",
     keywords="pyside6 material widgets",
     author="zhiyiYo",
     author_email="shokokawaii@outlook.com",
